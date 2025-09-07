80a48cdce128d31a7969452be14abfe346bcc4be
Andrew Ferreira <andrew.shien2@gmail.com>
2025-09-07 11:58:29 -0300

    fix: add settings header

--- src/views/settings/index.vue
+++ src/views/settings/index.vue
@@ -1,22 +1,38 @@
 <script setup lang="ts">
 import Tauri from './Tauri.vue';
 import Website from './Website.vue';
-import { asyncRef } from '@tb-dev/vue';
+import { useTemplateRef } from 'vue';
+import { toPixel } from '@tb-dev/utils';
 import { getServerAddr } from '@/commands';
 import { isTauri } from '@tauri-apps/api/core';
+import { asyncRef, useHeightDiff } from '@tb-dev/vue';
+import { SidebarTrigger } from '@tb-dev/vue-components';
 
 const { state: server } = asyncRef(null, getServerAddr);
+
+const topbar = useTemplateRef('topbarEl');
+const contentHeight = useHeightDiff(topbar);
 </script>
 
 <template>
-  <div class="relative flex size-full flex-col gap-6 p-4 overflow-x-hidden overflow-y-auto">
-    <div v-if="isTauri() && server" class="absolute right-4 bottom-2">
-      <span class="text-muted-foreground text-sm">{{ server }}</span>
+  <div class="flex size-full flex-col gap-2">
+    <div ref="topbarEl" class="flex h-14 w-full items-center justify-between px-2 md:px-6 py-4">
+      <div class="flex items-center justify-start gap-2">
+        <SidebarTrigger />
+        <h1 class="text-2xl font-semibold">Settings</h1>
+      </div>
+
+      <div v-if="isTauri() && server">
+        <span class="text-muted-foreground text-sm">{{ server }}</span>
+      </div>
     </div>
 
-    <h1 class="text-2xl font-semibold">Settings</h1>
-
-    <Tauri v-if="isTauri()" />
-    <Website v-else />
+    <div
+      class="flex flex-col gap-2 md:gap-4 overflow-x-hidden overflow-y-auto px-1 md:px-6 pb-12"
+      :style="{ height: toPixel(contentHeight) }"
+    >
+      <Tauri v-if="isTauri()" />
+      <Website v-else />
+    </div>
   </div>
 </template>
