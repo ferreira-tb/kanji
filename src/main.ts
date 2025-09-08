0255ce898ab52e268bdf0aeb535c4356248b9f8b
Andrew Ferreira <andrew.shien2@gmail.com>
2025-09-08 18:38:08 -0300

    fix: avoid push outside tauri

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ import '@tb-dev/vue-components/style';
 import '@/lib/prototype';
 import App from '@/App.vue';
 import { createApp } from 'vue';
+import { router } from '@/router';
 import { createPinia } from 'pinia';
-import { go, router } from '@/router';
 import { handleError } from '@/lib/error';
 import { isTauri } from '@tauri-apps/api/core';
 import { checkForUpdates } from '@/lib/updater';
@@ -32,7 +32,20 @@ setErrorHandler(handleError, app);
 app.use(router);
 app.use(pinia);
 
-checkForUpdates()
-  .then(() => go('home'))
-  .then(() => app.mount('#app'))
-  .err();
+async function init() {
+  try {
+    await checkForUpdates();
+  }
+  catch (err) {
+    handleError(err);
+  }
+  finally {
+    if (isTauri()) {
+      await router.push({ name: 'home' satisfies Route });
+    }
+
+    app.mount('#app');
+  }
+}
+
+void init();
