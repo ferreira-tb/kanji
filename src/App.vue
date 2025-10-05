60970da37ad61405e18b9aa361757d6be99d3a82
Andrew Ferreira <andrew.shien2@gmail.com>
2025-10-05 16:54:20 -0300

    feat: request fullscreen

--- src/App.vue
+++ src/App.vue
@@ -28,6 +28,13 @@ onMounted(async () => {
   try {
     await commands.createTrayIcon();
     await commands.showWindow();
+
+    if (!isTauri() && window.screen.width < 720) {
+      const main = document.querySelector('main');
+      if (main && !document.fullscreenElement) {
+        await main.requestFullscreen({ navigationUI: 'hide' });
+      }
+    }
   }
   catch (err) {
     handleError(err);
