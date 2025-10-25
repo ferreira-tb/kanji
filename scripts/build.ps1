738bdaad43a5bd380f58f5b53c446528cada5105
Andrew Ferreira <andrew.shien2@gmail.com>
2025-10-25 01:33:26 -0300

    chore: update dependencies

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -7,6 +7,12 @@ $PSNativeCommandUseErrorActionPreference = $true
 
 if ($Android) {
   cargo tauri android build --apk true
+
+  $PackageJson = Get-Content -Path 'package.json' -Raw | ConvertFrom-Json -AsHashtable
+  $Version = $PackageJson['version']
+
+  $Path = 'src-tauri/gen/android/app/build/outputs/apk/universal/release/app-universal-release.apk'
+  kanata add -p $Path -n "kanji-$Version.apk"
 }
 else {
   cargo tauri build
