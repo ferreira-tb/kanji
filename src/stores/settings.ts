2c0587d230f401eee8ef8bea3206bc6070d3f5ab
Andrew Ferreira <andrew.shien2@gmail.com>
2025-11-19 17:19:39 -0300

    fix: save base url to local storage

--- src/stores/settings.ts
+++ src/stores/settings.ts
@@ -1,8 +1,10 @@
-import { ref } from 'vue';
 import { defineStore } from 'pinia';
+import { ref, watchEffect } from 'vue';
+
+export const BASE_URL_KEY = 'kanji:base-url';
 
 export const useSettingsStore = defineStore('settings', () => {
-  const baseUrl = ref<string>();
+  const baseUrl = ref(localStorage.getItem(BASE_URL_KEY));
   const clipboard = ref(true);
 
   const editor = ref(__DEFAULT_SETTINGS__.editor);
@@ -15,6 +17,12 @@ export const useSettingsStore = defineStore('settings', () => {
 
   const setFileName = ref(__DEFAULT_SETTINGS__.setFileName);
   const setChunkSize = ref(__DEFAULT_SETTINGS__.setChunkSize);
+
+  if (__MOBILE__) {
+    watchEffect(() => {
+      localStorage.setItem(BASE_URL_KEY, baseUrl.value ?? '');
+    });
+  }
 
   return {
     baseUrl,
