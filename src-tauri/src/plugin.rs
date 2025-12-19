507c5b714afb35f4ac988efe59565df50e7ad3a8
Andrew Ferreira <andrew.shien2@gmail.com>
2025-12-19 13:59:03 -0300

    chore: update dependencies (#21)

--- src-tauri/src/plugin.rs
+++ src-tauri/src/plugin.rs
@@ -1,4 +1,3 @@
-use anyhow::Result;
 use tauri::Wry;
 use tauri::plugin::TauriPlugin;
 
@@ -41,7 +40,7 @@ pub fn prevent_default() -> TauriPlugin<Wry> {
 pub fn single_instance() -> TauriPlugin<Wry> {
   tauri_plugin_single_instance::init(|app, _, _| {
     let window = app.main_window();
-    let _: Result<()> = try {
+    let _ = try {
       window.show()?;
       window.unminimize()?;
       window.set_focus()?;
